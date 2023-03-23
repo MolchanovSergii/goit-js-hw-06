867ec132524159f74babd8acd77de1d90d5c60fb
MolchanovSergii <molchanse@gmail.com>
2023-03-23 21:20:27 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+const markup = ingredients.map(element => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("item");
+  listItem.style.listStyle = "none";
+  listItem.textContent = element;
+  return listItem;
+});
+
+list.append(...markup);
