6b8da8fbfbeda3ca1fef3f0daebae53bf3597c2c
MolchanovSergii <molchanse@gmail.com>
2023-03-23 18:48:55 +0200

    task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,17 @@
 const list = document.querySelector('#categories');
 
-console.dir(list);
+console.log(`Number of categories: ${list.children.length}`);
+console.log(" ");
+
+[...list.children].forEach(element => {
+    const category = element.querySelector('h2').textContent;
+    const item = element.querySelectorAll(".item li").length;
+    
+    
+    console.log(`Category: ${category}`);
+    console.log("Elements: ",item);
+    console.log(" ");
+});
+
+
+
