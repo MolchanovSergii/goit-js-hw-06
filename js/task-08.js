2df4fb6e2c5b452b0a7aae231089d4d396ee0925
MolchanovSergii <molchanse@gmail.com>
2023-03-25 00:14:17 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const ourForm = document.querySelector(".login-form");
 
+ourForm.addEventListener("submit", sendForm);
+
+function sendForm(event) {
+    event.preventDefault();
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+
+    if (email === "" || password === "") {
+        return alert ("всі поля повинні бути заповнені");
+    }
+
+    const elements = {
+        email,
+        password
+    };
+
+    console.log(`користувач ввів адрес єл.скриньки: ${email}, та пароль до неї: ${password}`);
+
+    ourForm.reset();
+}
