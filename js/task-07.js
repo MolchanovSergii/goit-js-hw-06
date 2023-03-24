46f024163c5fec61b1a4ef0372b921a5c9fdade1
MolchanovSergii <molchanse@gmail.com>
2023-03-24 23:16:27 +0200

    task7

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,6 @@
 const ourControl = document.querySelector("#font-size-control");
 const ourText = document.querySelector("#text");
 
-console.log(ourControl)
-console.log(ourText)
-
-
 ourControl.addEventListener("input", changeSize);
 
 function changeSize(event) {
