8c0925c29f22b4a7326cee7157cc2f9f9db551b2
MolchanovSergii <molchanse@gmail.com>
2023-03-25 00:43:44 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const ourBtn = document.querySelector(".change-color");
+const colorNow = document.querySelector(".color");
+const ourBody = document.body;
+
+ourBtn.addEventListener("click", changeColor);
+
+function changeColor(event) {
+  const ourNumber = prompt("введите любое число");
+
+  const chooseColor = getRandomHexColor(ourNumber);  
+
+  ourBody.style.backgroundColor = chooseColor;
+  colorNow.textContent = chooseColor;
+}
